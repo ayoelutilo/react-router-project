bb0eba34f94e1f3b876997de46ad918565701b49
Ayodele Elutilo <ayoelutilo@gmail.com>
2021-07-24 02:00:47 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import HomeCard from './components/HomeCard';
+import AboutCard from './components/AboutCard';
+import CuisineCard from './components/CuisineCard';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="App-header">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">About</Link>
+          </li>
+          <li>
+            <Link to="/">Cuisine</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/cuisine">
+            <Cuisine />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </nav>
     </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <HomeCard />
+}
+
+function About() {
+  return <AboutCard />
+}
+
+function Cuisine() {
+  return <CuisineCard />
 }
 
 export default App;
